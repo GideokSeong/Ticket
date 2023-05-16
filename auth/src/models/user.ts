12ec3167388c75010435cc848e3c658e1938d5f0
Gideok Seong <gseong@my.bridgeport.edu>
2023-05-15 21:52:18 -0400

    user model 05152023

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,12 +1,24 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // An interface that describes the properties
 // that are required to create a new User
-
 interface UserAttrs {
   email: string;
   password: string;
 }
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string,
+  password: string
+};
 
 const userSchema = new mongoose.Schema({
   email:{
@@ -18,15 +30,18 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 });
+userSchema.pre('save', async function(done){
+  if (this.isModified('password')){
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);  
+  }
+  done();
+});
 
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs: UserAttrs) =>{
+userSchema.statics.build = (attrs: UserAttrs) =>{
   return new User(attrs);
-}
+};
 
-buildUser({
-  email: 'test@test.com',
-  password: '123456'
-})
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
 export { User };

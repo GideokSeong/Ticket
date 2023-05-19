5f879f4d7ca3e96aa2b2863d7609ea01f7d27d44
Gideok Seong <gseong@my.bridgeport.edu>
2023-05-19 17:48:40 -0400

    Auth for user sign up / sign in 05192023

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,16 @@
-export default () => {
-  return <h1>Landing Page</h1>;
+import axios from 'axios'; 
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are Not signed in</h1>
 };
+
+LandingPage.getInitialProps = async (context) => {
+  console.log('Landing Page');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;

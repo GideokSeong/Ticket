e0bf8a7c172b09f716f508c55ea1e78eb81dfd83
Gideok Seong <gseong@my.bridgeport.edu>
2023-05-20 16:01:34 -0400

    Client part started

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 
-import { validateRequest } from '../middlewares/validate-request';
+import { validateRequest, BadRequestError } from '@gdtickets/common';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 import * as jwt from 'jsonwebtoken';
 
 const router = express.Router();

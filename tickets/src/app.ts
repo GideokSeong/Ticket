1b4dc0eea63d37c7572044df6b0c6fd822140bdc
Gideok Seong <gseong@my.bridgeport.edu>
2023-05-20 20:35:20 -0400

    tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { NotFoundError, errorHandler } from '@gdtickets/common';
+import { NotFoundError, errorHandler, currentUser } from '@gdtickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -10,9 +11,11 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: true
+    secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all('*', async ()=>{
   throw new NotFoundError();

0479e678022eab1d110189ae383de32f7b8b5b13
Gideok Seong <gseong@my.bridgeport.edu>
2023-05-23 15:31:54 -0400

    test for ticket

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,8 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { NotFoundError, errorHandler, currentUser } from '@gdtickets/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,6 +18,8 @@ app.use(
 );
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', async ()=>{
   throw new NotFoundError();
--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,10 +1,12 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import mongoose from 'mongoose';
 
 it('returns a 404 if the ticket is not found', async() =>{
-  await request(app)
-    .get('/api/tickets/asdfasdf')
+  const id = new mongoose.Types.ObjectId().toHexString();
+  const response = await request(app)
+    .get(`/api/tickets/${id}`)
     .send()
     .expect(404);
 });

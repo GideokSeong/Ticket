23a495ca3ba1cf1594098f057365f1bfc03393e8
Gideok Seong <gseong@my.bridgeport.edu>
2023-06-20 17:49:28 -0400

    testing

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from "../../app";
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route hanlder listening to /api/tickets for post request', async () => {
   const response = await request(app)
@@ -75,3 +76,17 @@ it('creates a ticket with valid inputs', async () => {
   expect(tickets.length).toEqual(1);
   expect(tickets[0].price).toEqual(20);
 })
+
+it('publishes an event', async() =>{
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'asdfasdf',
+      price: 20
+    })
+    .expect(201);
+  
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
